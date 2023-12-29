a520cbfb54ce4205a979f1baa78d429eeb680c10
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-29 21:21:19 +0100

    about fixed

--- src/app/components/AboutUs/AboutUs.js
+++ src/app/components/AboutUs/AboutUs.js
@@ -7,25 +7,6 @@ import theme from '@/app/theme/theme'
 import about1 from './about1.jpg'
 import about2 from './about2.jpg'
 
-const arrayItems = [
-  {
-    id: 1,
-    img: about1,
-    alt: 'picture of food'
-  },
-  {
-    id: 2,
-    subtitle: 'Taste perception',
-    title: 'TRADITIONAL & MODERN',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, culpa.'
-  },
-  {
-    id: 3,
-    img: about2,
-    alt: 'picture of food'
-  }
-]
-
 export const AboutUs = (props) => {
   const {
     sx,
@@ -73,83 +54,92 @@ export const AboutUs = (props) => {
           container
           spacing={2}
         >
-          {
-            arrayItems.map((item) => {
-              return (
-                <Grid
-                  item
-                  xs={12}
-                  md={4}
-                  key={item.id}
-                  sx={{
-                    width: '100%'
-                  }}
-                >
-                  {
-                    item.img &&
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        width: '100%'
-                      }}
-                    >
-                      <Image
-                        src={item.img}
-                        width={0}
-                        height={0}
-                        alt={item.alt}
-                        priority
-                        style={{
-                          width: '100%',
-                          objectFit: 'cover',
-                          height: '500px'
-                        }}
-                      />
-                    </Box>
-                  }
-                  {
-                    item.title &&
-                    <Box
-                      sx={{
-                        height: '500px',
-                        padding: '20px',
-                        textAlign: 'center',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        backgroundColor: theme.palette.background.secondBackgroundColor
-                      }}
-                    >
-                      <Typography
-                        variant={'subtitle1'}
-                        fontWeight={theme.typography.fontWeightBold}
-                        fontFamily={theme.typography.fontFamily[0]}
-                        color={theme.palette.text.secondary}
-                      >
-                        {item.subtitle}
-                      </Typography>
-                      <Typography
-                        variant={'h3'}
-                        sx={{
-                          marginBottom: '40px'
-                        }}
-                        fontWeight={theme.typography.fontWeightBold}
-                      >
-                        {item.title}
-                      </Typography>
-                      <Typography
-                        variant={'body1'}
-                        color={theme.palette.text.secondary}
-                      >
-                        {item.description}
-                      </Typography>
-                    </Box>
-                  }
-                </Grid>
-              )
-            })
-          }
+          <Grid
+            item
+            sm={12}
+            md={4}
+            sx={{
+              display: 'flex'
+            }}
+          >
+            <Image
+              src={about1}
+              height={0}
+              width={0}
+              priority
+              style={{
+                width: '100%',
+                height: '500px',
+                objectFit: 'cover'
+              }}
+              alt={'picture of food'}
+            />
+          </Grid>
+          <Grid
+            item
+            sm={12}
+            md={4}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                height: '500px',
+                padding: '20px',
+                textAlign: 'center',
+                backgroundColor: theme.palette.background.secondBackgroundColor
+              }}
+            >
+              <Typography
+                variant={'subtitle1'}
+                fontWeight={theme.typography.fontWeightBold}
+                fontFamily={theme.typography.fontFamily[0]}
+                color={theme.palette.text.secondary}
+              >
+                Taste perception
+              </Typography>
+              <Typography
+                variant={'h1'}
+                sx={{
+                  marginBottom: '40px'
+                }}
+                fontWeight={theme.typography.fontWeightBold}
+              >
+                TRADITIONAL & MODERN
+              </Typography>
+              <Typography
+                variant={'body1'}
+                color={theme.palette.text.secondary}
+                sx={{
+                  marginBottom: '20px'
+                }}
+              >
+                LLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, culpa.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            sm={12}
+            md={4}
+            sx={{
+              display: 'flex'
+            }}
+          >
+            <Image
+              src={about2}
+              height={0}
+              width={0}
+              style={{
+                width: '100%',
+                height: '500px',
+                objectFit: 'cover'
+              }}
+              alt={'picture of food'}
+            />
+          </Grid>
         </Grid>
       </Container>
     </Box>
