4235a43dc3baedba6e793a328d9f544b2ce17c62
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-30 18:46:18 +0100

    emailjs added

--- src/app/components/Contact/Contact.js
+++ src/app/components/Contact/Contact.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React, { useRef } from 'react'
+import { Box, Typography, TextField, Button } from '@mui/material'
 
-import { useForm, FormProvider } from 'react-hook-form'
+import theme from '@/app/theme/theme'
+
+import isEmail from 'validator/lib/isEmail'
+
+import emailjs from '@emailjs/browser'
+
+import { useForm } from 'react-hook-form'
 
 import contactBgI from './contactBgI.jpg'
-
-import ContactForm from '../ContactForm'
 
 export const Contact = (props) => {
   const {
@@ -13,12 +17,65 @@ export const Contact = (props) => {
     ...otherProps
   } = props
 
+  const form = useRef()
+
+  const sendEmail = async () => {
+    try {
+      const result = await emailjs.sendForm(
+        process.env.NEXT_PUBLIC_SERVICEID,
+        process.env.NEXT_PUBLIC_TEMPLATEID,
+        form.current,
+        process.env.NEXT_PUBLIC_PUBLICKEY
+      )
+      console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const methods = useForm()
-  const { handleSubmit, reset } = methods
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors }
+  } = methods
+
+  const registeredNameProps = register('customerName', {
+    required: {
+      value: true,
+      message: 'name is required'
+    }
+  })
+  const registeredPhoneNumberProps = register('mobileNumber', {
+    required: {
+      value: true,
+      message: 'mobile phone is required'
+    }
+  })
+  const registeredEmailProps = register('emailAddres', {
+    required: {
+      value: true,
+      message: 'email is required'
+    },
+    validate: (email) => isEmail(email) || 'Wrong Email'
+  })
+  const registeredPostcodeProps = register('postcode', {
+    required: {
+      value: true,
+      message: 'postocde is required'
+    }
+  })
+  const registeredMessageProps = register('message', {
+    required: {
+      value: true,
+      message: 'message is required'
+    }
+  })
 
   const onSubmit = handleSubmit(
     (data, e) => {
-      console.log(data, 'data')
+      sendEmail()
       reset()
     },
     (errors, e) => {
@@ -44,13 +101,125 @@ export const Contact = (props) => {
       }}
       {...otherProps}
     >
-      <FormProvider
-        {...methods}
+      <Box>
+        <Typography
+          variant={'subtitle1'}
+          fontWeight={theme.typography.fontWeightBold}
+          fontFamily={theme.typography.fontFamily[0]}
+          color={theme.palette.text.secondary}
+        >
+          get in touch
+        </Typography>
+        <Typography
+          variant={'h1'}
+          fontWeight={theme.typography.fontWeightBold}
+        >
+          CONTACT US
+        </Typography>
+      </Box>
+      <Box
+        component={'form'}
+        onSubmit={onSubmit}
+        sx={{
+          ...sx,
+          backgroundColor: theme.palette.background.firstBackgroundColor,
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '20px',
+          width: '100%',
+          maxWidth: '600px',
+          borderRadius: '5px',
+          textAlign: 'center'
+        }}
+        ref={form}
+        noValidate
+        autoComplete={'off'}
+        {...otherProps}
       >
-        <ContactForm
-          onSubmit={onSubmit}
+        <TextField
+          size={'small'}
+          margin={'normal'}
+          error={!!errors.customerName}
+          id={'customerName'}
+          label={'Name'}
+          helperText={errors.customerName?.message}
+          sx={{
+            '& .MuiInputBase-root': {
+              backgroundColor: theme.palette.background.secondBackgroundColor
+            }
+          }}
+          {...registeredNameProps}
         />
-      </FormProvider>
+        <TextField
+          size={'small'}
+          margin={'normal'}
+          error={!!errors.mobileNumber}
+          id={'mobileNumber'}
+          label={'Mobile Number'}
+          helperText={errors.mobileNumber?.message}
+          sx={{
+            '& .MuiInputBase-root': {
+              backgroundColor: theme.palette.background.secondBackgroundColor
+            }
+
+          }}
+          {...registeredPhoneNumberProps}
+        />
+        <TextField
+          size={'small'}
+          margin={'normal'}
+          error={!!errors.emailAddres}
+          id={'emailAddres'}
+          label={'Email Addres'}
+          helperText={errors.emailAddres?.message}
+          sx={{
+            '& .MuiInputBase-root': {
+              backgroundColor: theme.palette.background.secondBackgroundColor
+            }
+          }}
+          {...registeredEmailProps}
+        />
+        <TextField
+          size={'small'}
+          margin={'normal'}
+          error={!!errors.postcode}
+          id={'postcode'}
+          label={'Your Postecode'}
+          helperText={errors.postcode?.message}
+          sx={{
+            '& .MuiInputBase-root': {
+              backgroundColor: theme.palette.background.secondBackgroundColor
+            }
+          }}
+          {...registeredPostcodeProps}
+        />
+        <TextField
+          size={'small'}
+          margin={'normal'}
+          error={!!errors.message}
+          id={'message'}
+          label={'Your Message'}
+          multiline
+          rows={4}
+          helperText={errors.message?.message}
+          sx={{
+            '& .MuiInputBase-root': {
+              backgroundColor: theme.palette.background.secondBackgroundColor
+            }
+          }}
+          {...registeredMessageProps}
+        />
+        <Button
+          type={'submit'}
+          variant={'contained'}
+          sx={{
+            margin: 'auto',
+            color: 'white'
+          }}
+        >
+          SEND
+        </Button>
+      </Box>
 
     </Box>
   )
