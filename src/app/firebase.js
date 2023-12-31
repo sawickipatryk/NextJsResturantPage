49c4bf08cb81e2dfead21a333da2f5d23ca2f965
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-31 16:15:45 +0100

    firebase deployed

---
+++ src/app/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBjMoXla17HpVX5YpithcItFjWkhUzYFnw',
+  authDomain: 'website-restaurant-page2.firebaseapp.com',
+  projectId: 'website-restaurant-page2',
+  storageBucket: 'website-restaurant-page2.appspot.com',
+  messagingSenderId: '818879780396',
+  appId: '1:818879780396:web:d767cfd83734a3c033a389'
+}
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig)
